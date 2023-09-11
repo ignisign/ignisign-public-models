22b05e1b268905fb815e32024ec94f251ed03f5f
ThibaudG <thibaud@ignisign.io>
2023-09-11 10:30:32 +0200

    validator & adding an error

--- src/webhooks/webhook-responses.public.ts
+++ src/webhooks/webhook-responses.public.ts
@@ -1,4 +1,5 @@
-import { IsEnum, IsOptional, IsString } from "class-validator";
+import { IsEnum, IsOptional, IsString, ValidateNested } from "class-validator";
+import { Type } from "class-transformer";
 import { IGNISIGN_APPLICATION_ENV } from "../applications/applications.public";
 import { IGNISIGN_DOCUMENT_TYPE } from "../documents/document-entities.public";
 import { IGNISIGN_DOCUMENT_REQUEST_TARGET } from "../documents/document-request.public";
@@ -10,17 +11,39 @@ import { IGNISIGN_SIGNER_CREATION_INPUT_REF } from "../signers/signers.public";
 
 
 export class IgnisignWebhookDto_SignatureSession {
+  @IsString()
   signerId: string
+
+  @IsString()
   signatureRequestId: string
 }
 
 export class IgnisignWebhookDto_SignerDescription {
-  signerId  : string;
+  @IsString()
+  signerId        : string;
+
+  @IsOptional()
+  @IsString()
   lastName?       : string;
+
+  @IsOptional()
+  @IsString()
   firstName?      : string;
+
+  @IsOptional()
+  @IsString()
   email?          : string;
+
+  @IsOptional()
+  @IsString()
   phoneNumber?    : string;
+
+  @IsOptional()
+  @IsString()
   tokenValue?     : string;
+
+  @IsOptional()
+  @IsString()
   signerSecret?   : string;
 }
 export class IgnisignWebhookDto_SignatureSessionResult {
@@ -32,63 +55,118 @@ export class IgnisignWebhookDto_SignatureSessionResult {
 }
 
 export class IgnisignWebhookDto_DocumentRequest {
+  @IsString()
   documentId     : string;
+
+  @IsString()
   documentNature : string;
+
+  @IsString()
   mimeType       : string;
 }
 
 export class IgnisignWebhookDto_SignatureProofGenerated {
+  @IsString()
   appId              : string;
+
+  @IsEnum(IGNISIGN_APPLICATION_ENV)
   appEnv             : IGNISIGN_APPLICATION_ENV;
+
+  @IsString()
   documentId         : string;
+
+  @IsString()
   signatureRequestId : string;
 }
 
 export class IgnisignWebhookDto_Signer {
+  @IsString()
   appId       : string;
+
+  @IsEnum(IGNISIGN_APPLICATION_ENV)
   appEnv      : IGNISIGN_APPLICATION_ENV;
+
+  @IsString()
   signerId    : string;
+
+  @IsEnum(IGNISIGN_SIGNER_CREATION_INPUT_REF, { each: true })
   inputs      : IGNISIGN_SIGNER_CREATION_INPUT_REF[]
 }
 
 export class IgnisignWebhookDto_AdvancedProofGenerated {
+  @IsString()
   appId              : string;
+
+  @IsEnum(IGNISIGN_APPLICATION_ENV)
   appEnv             : IGNISIGN_APPLICATION_ENV;
+
+  @IsString()
   documentId         : string;
+
+  @IsString()
   signatureRequestId : string;
 }
 
 export class IgnisignWebhookDto_SignatureImageGenerated {
+  @IsString()
   appId              : string;
+
+  @IsEnum(IGNISIGN_APPLICATION_ENV)
   appEnv             : IGNISIGN_APPLICATION_ENV;
+
+  @IsString()
   documentId         : string;
+
+  @IsString()
   signatureRequestId : string;
+
+  @IsString()
   signerId           : string;
 }
 
 export class IgnisignWebhookDto_SignatureRequest {
+  @IsString()
   signatureRequestId  : string;
+
+  @IsOptional()
+  @IsString()
   externalId         ?: string;
 }
+
+export class IgnisignWebhookDto_SignatureRequestLaunchedSigner {
+  signerId   : string;
+  externalId : string;
+  token      : string;
+}
 export class IgnisignWebhookDto_SignatureRequestLaunched extends IgnisignWebhookDto_SignatureRequest {
-  signers : {
-    signerId   : string;
-    externalId : string;
-    token      : string;
-  }[]
+  @ValidateNested({ each: true })
+  @Type(() => IgnisignWebhookDto_SignatureRequestLaunchedSigner)
+  signers: IgnisignWebhookDto_SignatureRequestLaunchedSigner[]
 }
 
 export class IgnisignWebhookDto_SignatureProfileCreated {
+  @IsString()
   signatureProfileId  : string;
 }
 
 
 export class IgnisignWebhookDto_Signature {
+  @IsString()
   appId           : string;
+
+  @IsEnum(IGNISIGN_APPLICATION_ENV)
   appEnv          : IGNISIGN_APPLICATION_ENV;
+
+  @IsEnum(IGNISIGN_DOCUMENT_TYPE)
   documentNature  : IGNISIGN_DOCUMENT_TYPE;
+
+  @IsString()
   signerId        : string;
+  
+  @IsString()
   documentId      : string;
+
+  @IsString()
   signatureId     : string;
 }
 
