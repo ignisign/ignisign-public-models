93cdd17dff86f4e0135a0c817f4496c8c13dda45
Loic-Fritsch <loic.fritsch.fr.com>
2024-01-18 17:33:14 +0100

    add-document-authenticity-validator

--- src/_commons/basic-containers.public.ts
+++ src/_commons/basic-containers.public.ts
@@ -1,6 +1,14 @@
-import {  IsString } from "class-validator";
+import {  IsBoolean, IsString } from "class-validator";
 
 export class IgnisignJwtContainer {
   @IsString()
   jwtToken : string;
+}
+
+export class IgnisignDocument_AuthenticityValidationContainer {
+  @IsString()
+  documentId : string;
+
+  @IsBoolean()
+  authenticityValidated : boolean;
 }
