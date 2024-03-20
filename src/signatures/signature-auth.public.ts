1a4154d4c516d1bec1412ddc610cd901e442eee1
Paul <paul@pccc.lu>
2024-03-20 09:51:05 +0100

    gf

--- src/signatures/signature-auth.public.ts
+++ src/signatures/signature-auth.public.ts
@@ -5,6 +5,9 @@ export enum IGNISIGN_AUTH_FULL_MECHANISM_REF {
   TOTP                  = "TOTP",
   PASS_KEY_POSSESSION   = 'PASS_KEY_POSSESSION',
   AES_EID               = "AES_EID",
-  QES_EID               = "QES_EID"
+  QES_EID               = "QES_EID",
+  OPENID_CONNECT        = "OPENID_CONNECT",
+  SAML                  = "SAML",
+  AUTOMATE              = "AUTOMATE",
 }
 
