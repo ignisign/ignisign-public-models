3df58f3df89c13abf9cab457b10e19f874e9fd6f
Loic-Fritsch <loic.fritsch.fr.com>
2024-05-14 18:31:51 +0200

    wip

--- src/signers/signer-groups.public.ts
+++ src/signers/signer-groups.public.ts
@@ -1,11 +1,10 @@
-import { IsBoolean, IsEnum, IsNumber, IsObject, IsOptional, IsString, ValidateNested } from "class-validator";
+import { IsBoolean, IsEnum, IsNumber, IsObject, IsOptional, IsString } from "class-validator";
 import { IGNISIGN_SIGNATURE_METHOD_REF } from './../signatures/signature-methods.public';
 import { IGNISIGN_APPLICATION_ENV } from "../applications/applications.public";
 import { IGNISIGN_SIGNER_ENTITY_TYPE } from "./signers.public";
 import { IGNISIGN_INTEGRATION_MODE } from '../signatures/signatures.public';
 import { IGNISIGN_ID_PROOFING_METHOD_REF } from '../id-proofing/id-proofing-methods.public';
 import { IGNISIGN_AUTH_FULL_MECHANISM_REF } from '../signatures/signature-auth.public';
-import { Type } from "class-transformer";
 
 export class IgnisignSignerGroupSummary {
   @IsOptional()
@@ -50,6 +49,44 @@ export class IgnisignSignerGroupSummary {
   // TODO add class-validator
   signatureAuthMethods : { [key in IGNISIGN_SIGNATURE_METHOD_REF] :  IgnisignSignerGroup_SignatureAuthMethods[] };
 }
+
+export class IgnisignSignerGroupDto {
+  @IsString()
+  appId                    : string;
+
+  @IsEnum(IGNISIGN_APPLICATION_ENV)
+  appEnv                   : IGNISIGN_APPLICATION_ENV; 
+
+  @IsString()
+  name                     : string;
+  
+  @IsOptional()
+  @IsString()
+  description             ?: string;
+
+  @IsEnum(IGNISIGN_SIGNER_ENTITY_TYPE)
+  signerTypeAllowed        : IGNISIGN_SIGNER_ENTITY_TYPE[];
+
+  @IsBoolean()
+  isEmailSentByIgnisign    : boolean; // => mode ultime: => une gestion par EMAIL_TEMPLATE_ID => (avec des groups)
+
+  @IsBoolean()
+  isRecurrent              : boolean;
+
+  @IsEnum(IGNISIGN_INTEGRATION_MODE)
+  integrationMode  : IGNISIGN_INTEGRATION_MODE;
+
+  @IsOptional()
+  @IsString()
+  ssoConfigId ?: string;
+
+  @IsObject()
+  // @ValidateNested({ each: true })
+  // @Type(() => IgnisignSignerGroup_SignatureAuthMethods)
+  // TODO add class-validator
+  signatureAuthMethods : { [key in IGNISIGN_SIGNATURE_METHOD_REF] :  IgnisignSignerGroup_SignatureAuthMethods[] };
+}
+
 
 export enum IGNISIGN_SIGNER_TO_SIGNER_GROUP_STATUS {
   ACTIVE   = "ACTIVE",
