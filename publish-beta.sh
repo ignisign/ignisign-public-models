a2af7d63152a05d87951fe20164d3ee1b005d7da
Loic-Fritsch <loic.fritsch.fr.com>
2024-10-07 09:51:04 +0200

    add-publish-beta

---
+++ publish-beta.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+set -e
+
+rm -f "yarn.lock";
+echo "yarn.lock => Removed";
+
+rm -f "package-lock.json";
+echo "package-lock.json => Removed";
+
+rm -rf "node_modules";
+echo "node_modules => Removed";
+
+rm -rf "dist";
+echo "dist => Removed";
+
+rm -f "tsconfig.tsbuildinfo";
+echo "tsconfig.tsbuildinfo => Removed";
+
+yarn install;
+
+rm -f "tsconfig.tsbuildinfo";
+yarn build;
+
+npm publish --tag beta;
